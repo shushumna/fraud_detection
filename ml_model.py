72ca36e12dc8d798d0c7443f067ceab238408220
shushumna <shushu.chinnu@gmail.com>
2023-07-26 20:22:16 +0530

    added ml model1, model2 and model3 code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print("adding all ML related code here")
+
+
+def model1():
+    print("adding code for ml model1")
+
+def model2():
+    print("adding code for ml model2")
+
+def model3():
+    print("adding code for ml model3")
