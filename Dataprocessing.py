001148a96eea10ea0171c82aa0b9dddfab1954d5
shushumna <shushu.chinnu@gmail.com>
2023-07-26 19:33:00 +0530

    correct code

--- Dataprocessing.py
+++ Dataprocessing.py
@@ -1,3 +1,18 @@
 
 print("data related to the fraud detection")
 print("processed data")
+
+a=5
+b=7
+
+def sum(a,b):
+    c=a+b
+    return c
+
+print("addition function")
+
+def mul(a,b):
+    d=a+b
+    return b
+
+print("multiplication fuction")
